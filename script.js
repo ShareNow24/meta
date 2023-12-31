f75927861fc0461e859282b4bb403e9d3e4b2e3f
ShareNow24 <155320155+ShareNow24@users.noreply.github.com>
2024-01-01 01:43:20 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-function submitForm() {
+function submitForm(event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+    
     // Show wait popup on form submission
     const waitPopup = document.getElementById('waitPopup');
     waitPopup.style.display = 'block';
@@ -6,20 +8,9 @@ function submitForm() {
     // Replace the Formspree link below with your own link
     const formspreeLink = 'https://formspree.io/f/xqkrkppn';
     
-    // Form data
-    const formData = new FormData(document.getElementById('infoForm'));
-
-    // AJAX request to Formspree
-    const xhr = new XMLHttpRequest();
-    xhr.open('POST', formspreeLink, true);
-    xhr.setRequestHeader('Accept', 'application/json');
-
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            // Redirect to a new page after form submission
-            window.location.href = 'https://cdn.shopify.com/s/files/1/0558/7052/1432/files/images.jpg?v=1704050622';
-        }
-    };
-
-    xhr.send(formData);
+    // Simulate form submission delay
+    setTimeout(() => {
+        // Redirect to a new page after form submission
+        window.location.href = 'https://cdn.shopify.com/s/files/1/0558/7052/1432/files/images.jpg?v=1704050622';
+    }, 2000);
 }
